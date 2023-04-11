6a21e8389504a1850b6a11861b93485d1f871770
Nahuelsan <nahuelsan96@gmail.com>
2023-04-10 23:39:49 -0300

    Init steps

---
+++ src/pages/addOns/index.tsx
@@ -0,0 +1,5 @@
+const AddOns = () => {
+
+}
+
+export default AddOns;
---
+++ src/pages/selectPlan/index.tsx
@@ -0,0 +1,9 @@
+const SelectPlan = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default SelectPlan;
